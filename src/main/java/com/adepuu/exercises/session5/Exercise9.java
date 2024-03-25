d84b6fcbb72dcb564e9de41b42e9ac0dc50857e6
irfazbow <irfanefp@gmail.com>
2024-03-25 23:43:03 +0700

    First commit for Exercise 9 Note: -Input does not use a scanner

--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Exercise9 {
     /**
      * Java Program to get the number of days you have to wait after the i-th day to get a warmer temperature
@@ -16,7 +19,37 @@ public class Exercise9 {
      * Input: temperatures = [30,60,90]
      * Output: [1,1,0]
      */
+    static void daysToWarmer(int[] dataTemperatures) {
+        int n = dataTemperatures.length;
+        int[] result = new int[n];
+        int maxTemp = Integer.MIN_VALUE;
+
+        for (int tempereature : dataTemperatures) {
+            maxTemp = Math.max(maxTemp, tempereature);
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temperature = dataTemperatures[i];
+            int nextWarmerDay = Integer.MAX_VALUE;
+
+            for (int j = temperature + 1; j <= maxTemp; j++) {
+                if (map.containsKey(j)) {
+                    nextWarmerDay = Math.min(nextWarmerDay, map.get(j) - i);
+                }
+            }
+
+            if (nextWarmerDay != Integer.MAX_VALUE) {
+                result[i] = nextWarmerDay;
+            }
+            map.put(temperature, i);
+        }
+        System.out.println(Arrays.toString(result));
+    }
     public static void main(String[] args) {
+        int[] dataTemperatures = {23, 25, 26, 23, 24, 24, 22, 23, 25, 31, 32, 30, 33, 19};
+        daysToWarmer(dataTemperatures);
 
     }
 }
