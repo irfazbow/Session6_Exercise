4d94633fe2af5bf590bbea35a90e553aaf91e657
irfazbow <irfanefp@gmail.com>
2024-03-25 11:44:19 +0700

    First commit for Exercise 1. Note: -Input of the data is performed without a scanner. -The for used an enhanced for. -The method/function does not return a value, the value is directly printed from the function.

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,18 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    static void findMax(int[] dataArray) {
+        int dataMax = dataArray[0];
+        for (int i : dataArray) {
+            if (i > dataMax) {
+                dataMax = i;
+            }
+        }
+        System.out.println("The largest number from the data is: " + dataMax);
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray = {1, 2, 21, 4, 9, 5, 10, 11, 1, 5, 4, 7};
+        findMax(inputArray);
+    }
 }
