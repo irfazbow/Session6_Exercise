a482ce630bef44100447f9354261d0fd44620174
irfazbow <irfanefp@gmail.com>
2024-03-25 15:56:24 +0700

    First commit for Exercise 6. Note: -Input does not use a scanner. -Similar code with Exercise 4 by using a utility of Arrays.copyOf.

--- src/main/java/com/adepuu/exercises/session5/Exercise6.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise6.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise6 {
     /**
      * Java Array Program to Remove All Occurrences of an Element in an Array
@@ -11,7 +13,22 @@ public class Exercise6 {
      * [ 1, 2, 1, 3, 5, 1 ]  to
      * Final result: [2, 3, 5]
      */
+    static void removeTarget(int[] dataArray, int target) {
+        int[] newArray = new int[dataArray.length];
+        int newIndex = 0;
+
+        for (int i = 0; i < dataArray.length; i++) {
+            if (dataArray[i] != target) {
+                newArray[newIndex] = dataArray[i];
+                newIndex++;
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.copyOf(newArray, newIndex)));
+    }
     public static void main(String[] args) {
+        int[] inputArray = {1, 1, 3, 4, 11, 11, 9, 13, 15};
+        int inputTarget = 1;
+        removeTarget(inputArray, inputTarget);
 
     }
 }
