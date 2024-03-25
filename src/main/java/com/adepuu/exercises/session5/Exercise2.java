00b592886cfcf311ed4918c5a48c30f351a063b2
irfazbow <irfanefp@gmail.com>
2024-03-25 12:07:23 +0700

    First commit for Exercise 2. Note: -Input does not use a scanner. -Cannot input the data moved with a negative value, zero, or data moved higher than the available datum from the data set.

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -8,7 +10,34 @@ public class Exercise2 {
      * Explanation: d=2 so 2 elements are rotated to the end of the array. So, 1 2 is rotated back
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
+
+    static void moveToEnd(int[] dataArray, int dataMoved) {
+        if (dataArray.length < dataMoved) {
+            System.out.println("You're moving more data than the available data in the array. Do a valid input, please.");
+            return;
+        }
+        if (dataArray.length == dataMoved || dataMoved == 0) {
+            System.out.println("You basically do not move anything. The data sorting is still the same.");
+            return;
+        }
+        if (dataMoved < 0) {
+            System.out.println("We don't support a negative value for the number of moved data.");
+            return;
+        }
+
+        for (int i = 0; i < dataMoved; i++) {
+            int dataTemp = dataArray[0];
+            for (int j = 1; j < dataArray.length; j++) {
+                dataArray[j-1] = dataArray[j];
+            }
+            dataArray[dataArray.length - 1] = dataTemp;
+        }
+        System.out.println("The data with moved array: " + Arrays.toString(dataArray));
+    }
     public static void main(String[] args) {
+        int[] inputData = {1, 2, 3, 7, 8, 9};
+        int inputMove = 3;
+        moveToEnd(inputData, inputMove);
 
     }
 }
