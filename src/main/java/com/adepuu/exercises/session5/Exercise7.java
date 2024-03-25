061beb7e57745246096baa9dd35e3e441a3b7cac
irfazbow <irfanefp@gmail.com>
2024-03-25 16:20:49 +0700

    First commit for Exercise 7. Note: -The input does not use a scanner. -In a panic mode, running out of time, using utility of sorting for char arrays and equality of char arrays.

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise7 {
     /**
      * Java String Program to Check Anagram
@@ -12,7 +14,30 @@ public class Exercise7 {
      * Input: s = "rat", t = "car"
      * Output: false
      */
-    public static void main(String[] args) {
+    static void isAnagram (String s, String t) {
+        s = s.replaceAll("\\s","");
+        t = t.replaceAll("\\s","");
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+        if (s.length() != t.length()) {
+            System.out.println(false);
+            return;
+        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
 
+        if (Arrays.equals(sArray, tArray)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "Listen";
+        String t = "Silent";
+        isAnagram(s, t);
     }
 }
