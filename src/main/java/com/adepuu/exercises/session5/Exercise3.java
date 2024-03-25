b53f70ad5c049e9797afe52a9ccb2a193af34b3f
irfazbow <irfanefp@gmail.com>
2024-03-25 13:29:02 +0700

    First commit for Exercise 3. Note: -Input does not use a scanner.

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -16,7 +16,23 @@ public class Exercise3 {
      * Input: nums = [1,1,1,3,3,4,3,2,4,2]
      * Output: true
      */
-    public static void main(String[] args) {
 
+    static void isDuplicate(int[] dataArray) {
+        boolean isDup = false;
+        for (int i = 0; i < dataArray.length; i++) {
+            for (int j = i + 1; j < dataArray.length; j++) {
+                if (dataArray[i] == dataArray[j]) {
+                    isDup = true;
+                    System.out.println(isDup);
+                    return;
+                }
+            }
+        }
+        System.out.println(isDup);
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray = {1, 2, 3, 4, 5, 6, 11, 1};
+        isDuplicate(inputArray);
     }
 }
