a2d625c6395714186e81b9a91dcfea7403fe6b95
irfazbow <irfanefp@gmail.com>
2024-03-25 15:27:51 +0700

    First commit for Exercise 4. Note: -Input does not use a scanner. -Using an Arrays.copyOf utility for removing the excess index of the same or duplicate data.

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -8,7 +10,27 @@ public class Exercise4 {
      * Output: [ 1, 2, 3, 4, 5 ]
      * @param args
      */
-    public static void main(String[] args) {
+    static void removeDuplicate(int[] dataArray) {
+        int[] dataUnique = new int[dataArray.length];
+        int indexUnique = 0;
 
+        for (int i = 0; i < dataArray.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < indexUnique; j++) {
+                if (dataArray[i] == dataUnique[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                dataUnique[indexUnique] = dataArray[i];
+                indexUnique++;
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.copyOf(dataUnique, indexUnique)));
+    }
+    public static void main(String[] args) {
+        int[] inputArray = {31, 32, 31, 32, 1, 1, 2, 2, 1, 17, 16, 17};
+        removeDuplicate(inputArray);
     }
 }
