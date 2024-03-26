4195de87939a6e0e2bef48b75fe4de54d7e84b7e
irfazbow <irfanefp@gmail.com>
2024-03-26 14:45:35 +0700

    feat(exercise1) first commit new code
    note:
    -after input a data, the list of the input data will be shown, but this takes a long time
    -further optimization is required

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,18 +1,39 @@
 package com.adepuu.exercises.session6;
 
-public class Exercise1 {
-    /**
-     * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
-     *
-     * Input: 1, 2, 3, 4, 5, q
-     * Output: 3
-     *
-     * Input: 1, 2, 3, a, b, 4, 5, q
-     * Output: 3
-     *
-     * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
-     */
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Exercise1 {
+
+    static void inputAverage() {
+        ArrayList<Float> numbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        float sum = 0;
+        System.out.print("Input a number, or q to end the input: ");
+        while (true) {
+            input = scanner.next();
+            try {
+                float number = Float.parseFloat(input);
+                numbers.add(number);
+                System.out.println("Your current list of data: " + numbers);
+            } catch (NumberFormatException e) {
+                if (input.equalsIgnoreCase("q")) {
+                    for (int i = 0; i < numbers.size(); i++) {
+                        sum += numbers.get(i);
+                    }
+                    float average = sum / numbers.size();
+                    System.out.println("The total value is: " + sum);
+                    System.out.println("The average value is: " + average);
+                    break;
+                } else {
+                    System.out.println("Your input is not a number or q, please input properly.");
+                }
+            }
+        }
+        scanner.close();
+    }
+    public static void main(String[] args) {
+        inputAverage();
     }
 }
