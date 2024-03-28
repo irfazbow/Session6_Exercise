92b187230b033ef6f34119551b3835b7184cdf92
irfazbow <irfanefp@gmail.com>
2024-03-28 11:52:06 +0700

    feat(exercise, oop): ticketing system new version
    note:
    -major revision/change from the 2 previous version
    -there can be many tickets
    -bad, if another event is created, the user should be able to purchase a different ticket from another event, but this can only buy from 1 event
    -bad, the price on the ticket class does not feel like it's necessary to be there

--- src/main/java/com/adepuu/exercises/session7/TicketingSystem.java
+++ src/main/java/com/adepuu/exercises/session7/TicketingSystem.java
@@ -3,22 +3,56 @@ package com.adepuu.exercises.session7;
 import java.util.HashMap;
 
 public class TicketingSystem {
-
     public static void main(String[] args) {
+        Event event1 = new Event("Event 1");
+        Purchaser user1 = new Purchaser("Alibaba");
+        event1.purchaseTicket(user1, "Premium");
     }
 }
 
 class Event {
-    private HashMap<String, Ticket> eventTickets;
+    private HashMap<String, Integer> ticketsAvailable;
     private String eventName;
+    private int numID;
 
-    public Event() {
+    public Event(String eventName) {
         this.eventName = eventName;
-        this.eventTickets = new HashMap<>();
+        this.ticketsAvailable = new HashMap<>();
+        this.ticketsAvailable.put("Standard", 50);
+        this.ticketsAvailable.put("Premium", 20);
+        this.ticketsAvailable.put("Pertamax", 10);
+        this.numID = 0;
     }
 
-    public void createEvent(String eventName, Ticket ticket) {
-        eventTickets.put(eventName, ticket);
+    public String generateID(){
+        String ticketID = "ID_" + numID;
+        numID++;
+        return ticketID;
+    }
+
+    private double getPriceTicket(String type) {
+        switch (type) {
+            case "Standard": return 50000;
+            case "Premium": return 150000;
+            case "Pertamax": return 300000;
+            default: return 0;
+        }
+    }
+
+    public void purchaseTicket(Purchaser user, String type) {
+        if (ticketsAvailable.containsKey(type) && ticketsAvailable.get(type) > 0) {
+            if (user.hasPurchasedTicket()) {
+                System.out.println("Sorry, but one user can only purchase 1 ticket of any type.");
+                return;
+            }
+            int available = ticketsAvailable.get((type));
+            ticketsAvailable.put(type, available - 1);
+            double price = getPriceTicket(type);
+            String ticketID = generateID();
+            user.addTicket(new Ticket(type, ticketID, price), eventName, getPriceTicket(type));
+        } else {
+            System.out.println("Sorry, tickets for " + type + " type is not available or no longer available.");
+        }
     }
 }
 
@@ -26,35 +60,50 @@ class Ticket {
     private String ticketID;
     private String type;
     private double price;
-    private int originalQuantity;
 
 
-    public Ticket(String type, double price, int originalQuantity) {
+    public Ticket(String type, String ticketID, double price) {
         this.type = type;
+        this.ticketID = ticketID;
         this.price = price;
-        this.originalQuantity = originalQuantity;
-        this.ticketID = "ID_" +
-
     }
 
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
 
 class Purchaser {
     private String name;
+    private HashMap<String, Ticket> ticketUser;
+    private boolean purchasedTicket;
 
     public Purchaser(String name) {
         this.name = name;
+        this.ticketUser = new HashMap<>();
+        this.purchasedTicket = false;
     }
 
-    public void buyTicket(Ticket ticketType, int buyQuantity) {
-        if (buyQuantity <= ticketType.ticketStock) {
-            ticketType.ticketStock -= buyQuantity;
-            System.out.println("Your purchase has been confirmed.");
-        } else if (buyQuantity > ticketType.ticketStock) {
-            System.out.println("The buying quantity (" + buyQuantity + ") exceeds the remaining ticket. Available ticket is only " + ticketType.ticketStock);
-        } else {
-            System.out.println("We deeply apologize, the ticket has already been sold out.");
-        }
+    public void addTicket (Ticket ticket, String eventName, double price) {
+        ticketUser.put(ticket.getType(), ticket);
+        purchasedTicket = true;
+        System.out.println("A ticket has been purchased." +
+                "\nEvent name: " + eventName +
+                "\nTicket type: " + ticket.getType() +
+                "\nTicket ID: " + ticket.getTicketID() +
+                "\nPrice: Rp " + price +
+                "\nYour name: " + name);
+    }
 
+    public boolean hasPurchasedTicket() {
+        return purchasedTicket;
     }
 }
