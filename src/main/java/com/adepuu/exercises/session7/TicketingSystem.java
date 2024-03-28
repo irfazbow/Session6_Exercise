24231fe0fd4bd98b759ef21836703e2d6cacd077
irfazbow <irfanefp@gmail.com>
2024-03-28 08:59:12 +0700

    feat(exercise, oop): major revision change
    note:
    -revise as per detailed instruction
    -delete almost everything
    -under construction, commit to save for sync fork from the utmost main

--- src/main/java/com/adepuu/exercises/session7/TicketingSystem.java
+++ src/main/java/com/adepuu/exercises/session7/TicketingSystem.java
@@ -1,42 +1,42 @@
 package com.adepuu.exercises.session7;
 
+import java.util.HashMap;
+
 public class TicketingSystem {
 
     public static void main(String[] args) {
-        Ticket ticket1 = new Ticket("1","Collabora", 195000, 5000);
-        Ticket ticket2 = new Ticket("2","Jazz Goes to Campus", 100000, 1500);
-        Ticket ticket3 = new Ticket("3", "Orewa Oriza", 225000, 7500);
-        ticket1.printTicketInfo();
-        ticket2.printTicketInfo();
-        ticket3.printTicketInfo();
-        Purchaser purchaser1 = new Purchaser("Alison");
-        purchaser1.buyTicket(ticket1,5001);
-        ticket1.getTicketStock();
+    }
+}
+
+class Event {
+    private HashMap<String, Ticket> eventTickets;
+    private String eventName;
+
+    public Event() {
+        this.eventName = eventName;
+        this.eventTickets = new HashMap<>();
+    }
+
+    public void createEvent(String eventName, Ticket ticket) {
+        eventTickets.put(eventName, ticket);
     }
 }
 
 class Ticket {
     private String ticketID;
-    private String ticketEvent;
-    private int ticketPrice;
-    protected int ticketStock;
+    private String type;
+    private double price;
+    private int originalQuantity;
 
-    public Ticket(String ticketID, String ticketEvent, int ticketPrice, int ticketStock) {
-        this.ticketID = ticketID;
-        this.ticketEvent = ticketEvent;
-        this.ticketPrice = ticketPrice;
-        this.ticketStock = ticketStock;
+
+    public Ticket(String type, double price, int originalQuantity) {
+        this.type = type;
+        this.price = price;
+        this.originalQuantity = originalQuantity;
+        this.ticketID = "ID_" +
+
     }
 
-    public void printTicketInfo() {
-        System.out.println("Ticket ID: " + ticketID);
-        System.out.println("Event name: " + ticketEvent);
-        System.out.println("Price: Rp " + ticketPrice);
-        System.out.println("Remaining available tickets: " + ticketStock + " ea");
-    }
-    public void getTicketStock () {
-        System.out.println("The remaining ticket for Event " + ticketEvent + " is: " + ticketStock);
-    }
 }
 
 class Purchaser {
